2882d32797d9949955db2113ca82ecabe1f69b57
Paulius Zaleckas <paulius.zaleckas@gmail.com>
2013-01-22 16:48:35 +0200

    We don't use "fill" option anymore

--- src/defaults/preferences/prefs.js
+++ src/defaults/preferences/prefs.js
@@ -1,5 +1,4 @@
 pref("extensions.join-ng@paulius.zaleckas.description", "chrome://join-ng/locale/join-ng.properties");
 
 pref('extensions.join-ng.folder', 'chrome://join-ng/locale/join-ng.properties');
-pref('extensions.join-ng.fill', true);
 pref('extensions.join-ng.debug', false);
